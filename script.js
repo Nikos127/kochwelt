e44d32ad9870954e383b6535e07bdd946830aada
Nikos127 <119165557+Nikos127@users.noreply.github.com>
2026-01-04 10:40:51 +0100

    Portion input und vorgehensweise Erklärung

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function calculate() {
     const grundElement = document.getElementById('portions').value;
+    const input = document.getElementById('portions');
+
+    if (!input.checkValidity()) {
+        input.reportValidity();
+        return;
+    }
 
     document.querySelectorAll('.amount').forEach(el => {
         const basevalue = el.dataset.base ?? el.innerHTML;
@@ -7,6 +13,10 @@ function calculate() {
         el.innerHTML = (grundElement * basevalue);
     })
 };
+
+// Mit getElementById kann ich nur ein Element ansprechen. Beim googeln bin ich auf querySelectorAll gestoßen, womit ich alle Elemente selektieren kann – über eine Class. Danach habe ich ebenfalls per Google herausgefunden, dass ich die einzelnen Werte entweder mit einer Schleife oder mit forEach ändern kann. Das hat auch alles super funktioniert – bis ich einen Wert erneut ändern wollte. Dann wurde immer der jetzige Wert weiter vervielfacht. Aus meiner VBA-Erfahrung habe ich das so gelöst, dass ich den Anfangswert separat speichere.
+// Quellen: W3Schools und MDN. 
+
 
 function toggleRespMenu() {
     document.getElementById('resp_menu').classList.toggle('resp_menu_closed')
